3ccfd57aa9d4e6af0dea73deeb4562302dc77d75
Emmanuel Lacour <elacour@cpan.org>
2011-05-25 12:46:48 +0000

    Import of ELACOUR/RT-Extension-UserDetails-0.02 from CPAN.
    gitpan-cpan-distribution: RT-Extension-UserDetails
    gitpan-cpan-version:      0.02
    gitpan-cpan-path:         ELACOUR/RT-Extension-UserDetails-0.02.tar.gz
    gitpan-cpan-author:       ELACOUR
    gitpan-cpan-maturity:     released

--- lib/RT/Extension/UserDetails.pm
+++ lib/RT/Extension/UserDetails.pm
@@ -9,11 +9,11 @@ RT::Extension::UserDetails - allows to quickly display watchers personnal detail
 
 =head1 VERSION
 
-Version 0.01
+Version 0.02
 
 =cut
 
-our $VERSION = '0.01';
+our $VERSION = '0.02';
 
 
 =head1 SYNOPSIS
